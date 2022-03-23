6e3be0e09e5a239ac74b3ccc3ec7a1a170f8a263
Leo Costa <leolpc21@hotmail.com>
2022-03-23 11:31:35 -0300

    Atualização Segundo Cenário

--- src/test/java/TarefaBase2Camp/pages/DeletarReportIssue.java
+++ src/test/java/TarefaBase2Camp/pages/DeletarReportIssue.java
@@ -1,7 +1,18 @@
 package TarefaBase2Camp.pages;
 
 import TarefaBase2Camp.bases.PageBase;
+import org.openqa.selenium.By;
 
 public class DeletarReportIssue extends PageBase {
+
+    By myViewLinkText = By.linkText("My View");
+    By reportedByMeLinkText = By.linkText("Reported by Me");
+
+    public void clicarNoLinkMyView(){
+        click(myViewLinkText);
+    }
+    public void clicarNoLinkReportedByMe(){
+        click(reportedByMeLinkText);
+    }
 
 }
--- src/test/java/TarefaBase2Camp/tests/DeletarReportIsueTest.java
+++ src/test/java/TarefaBase2Camp/tests/DeletarReportIsueTest.java
@@ -3,18 +3,23 @@ package TarefaBase2Camp.tests;
 import TarefaBase2Camp.bases.TestBase;
 import TarefaBase2Camp.flows.CriarReportIssueFlows;
 import TarefaBase2Camp.flows.LoginFlows;
+import TarefaBase2Camp.pages.DeletarReportIssue;
 import org.junit.Test;
 
 public class DeletarReportIsueTest extends TestBase{
     LoginFlows loginFlows;
     CriarReportIssueFlows criarReportIssueFlows;
+    DeletarReportIssue deletarReportIssue;
 
     @Test
     public void deletarReport() {
         loginFlows = new LoginFlows();
         criarReportIssueFlows = new CriarReportIssueFlows();
+        deletarReportIssue = new DeletarReportIssue();
 
         loginFlows.efetuarLogin();
         criarReportIssueFlows.criarReport();
+        deletarReportIssue.clicarNoLinkMyView();
+        deletarReportIssue.clicarNoLinkReportedByMe();
     }
 }
