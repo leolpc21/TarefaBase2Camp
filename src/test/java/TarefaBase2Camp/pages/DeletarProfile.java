32d7d40c82e09f87091c361e08e160ba548860e7
Leo Costa <leolpc21@hotmail.com>
2022-03-26 20:16:25 -0300

    Atualização Quarto Cenário

--- src/test/java/TarefaBase2Camp/pages/DeletarProfile.java
+++ src/test/java/TarefaBase2Camp/pages/DeletarProfile.java
@@ -5,9 +5,19 @@ import org.openqa.selenium.By;
 
 public class DeletarProfile extends PageBase {
 
+    By linkMyAccont = By.linkText("My Account");
+    By linkProfiles = By.linkText("Profiles");
     By profileSelect = By.name("profile_id");
     By deleteProfileInput = By.xpath("//input[@value='delete']");
     By buttonSubmitInput = By.xpath("//input[@value='Submit']");
+
+    public void clicarNoLinkMyAccount() {
+        click(linkMyAccont);
+    }
+
+    public void clicarNoLinkProfiles() {
+        click(linkProfiles);
+    }
 
     public void selecionarOption(String opcao){
         comboBoxSelectByVisibleText(profileSelect, opcao);
